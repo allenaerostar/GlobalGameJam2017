ad01de1c4ef223a550e89a56bc84d429719826bb
RhainyLeo <liaohenry2310@gmail.com>
2017-01-21 02:54:27 -0500

    Implemented Score UI

--- Assets/henry's work/Scripts/ScoreCounter_script.cs
+++ Assets/henry's work/Scripts/ScoreCounter_script.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter_script : MonoBehaviour {
 
@@ -8,15 +9,19 @@ public class ScoreCounter_script : MonoBehaviour {
     public int teamBlueScore;
     public int teamRedScore;
 
+    public Text BlueScoreUI;
+    public Text RedScoreUI;
+
     void Start()
     {
         teamBlueScore = 0;
-        teamRedScore = 0;
+        teamRedScore = 0; 
     }
 
-    void Update()
+    void FixedUpdate()
     {
-
+        BlueScoreUI.text = teamBlueScore.ToString();
+        RedScoreUI.text = teamRedScore.ToString();
     }
 	
     void restartScore()
