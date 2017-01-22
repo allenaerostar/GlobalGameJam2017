14aa870f04a4654f8274c4ccdba792575f7c3e1e
RhainyLeo <liaohenry2310@gmail.com>
2017-01-22 01:45:33 -0500

    implemented ball explosion when scored

--- Assets/henry's work/Scripts/GeneralBall_script.cs
+++ Assets/henry's work/Scripts/GeneralBall_script.cs
@@ -18,9 +18,9 @@ public class GeneralBall_script : MonoBehaviour {
     //Freezes ball midair, plays explosion animation, then destroys ball after explosion animation ends.
     public void ballExplode()
     {
+        anim.SetTrigger("isExploding");
         ballAudio.PlayOneShot(explosionSFX);
         this.GetComponent<Rigidbody2D>().Sleep();
-        anim.SetTrigger("isExploding");
         Destroy(gameObject, explosion.length);
     }
 
