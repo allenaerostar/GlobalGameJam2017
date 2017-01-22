5c08db6d4c0adfeff0ada8897090087bb553d909
Leo <leojh.li@mail.utoronto.ca>
2017-01-22 13:15:58 -0500

    added restart button to game over screen (does nothing)

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartGame : MonoBehaviour {
+
+	private Button restartButton;
+
+	// Use this for initialization
+	void Start () {
+		restartButton = GetComponent<Button> ();
+		restartButton.onClick.AddListener (Restart);
+	}
+
+	void Restart(){
+		// restart the game here
+
+
+	}
+}
