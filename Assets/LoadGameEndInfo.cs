3422eb29a2d2d7201ed1c797c63828ddf12587af
Leo <leojh.li@mail.utoronto.ca>
2017-01-22 10:20:19 -0500

    changed ending screen and its script

---
+++ Assets/LoadGameEndInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadGameEndInfo : MonoBehaviour {
+
+	public Text winnerText;
+	public Text blueScore;
+	public Text redScore;
+	public Color blue;
+	public Color red;
+
+	public void SetInfo(string winner, string bScore, string rScore){
+		if (winner == "Blue") {
+			winnerText.color = blue;
+			winnerText.GetComponent<Text> ().text = "BLUE TEAM WINS";
+		} else if (winner == "Red") {
+			winnerText.color = red;
+			winnerText.GetComponent<Text> ().text = "RED TEAM WINS";
+		} else {
+			Debug.Log ("Invalid winner passed. Must be Red or Blue.");
+		}
+
+		blueScore.GetComponent<Text> ().text = bScore;
+		redScore.GetComponent<Text> ().text  = rScore;
+	}
+}
