49d13752fc086fa0098f38b11957959b22f17add
Leo <leojh.li@mail.utoronto.ca>
2017-01-21 19:09:37 -0500

    addded ending game screen with panels

---
+++ Assets/leo's stuff/LoadEndScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadEndScreen : MonoBehaviour {
+
+	public Text winnerText;
+	public Text redScore;
+	public Text blueScore;
+	public Color red;
+	public Color blue;
+
+	public GameObject player1;
+
+	// Use this for initialization
+	void Start () {
+		redScore.GetComponent<Text> ().text  = "2";
+		blueScore.GetComponent<Text> ().text  = "1000";
+
+		player1.GetComponent<LoadSummary> ().setStats ("2", "1m25s", "25", "14");
+		setWinnerBlue ();
+	}
+
+	void setWinnerRed() {
+		winnerText.color = red;
+		winnerText.GetComponent<Text> ().text = "RED TEAM WINS";
+	}
+
+	void setWinnerBlue() {
+		winnerText.color = blue;
+		winnerText.GetComponent<Text> ().text = "BLUE TEAM WINS";
+	}
+}
